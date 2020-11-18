189df844617e8f7ac2c35f90d7c0d074c53bad42
VeroMG <veromg86@hotmail.com>
2020-11-18 23:26:16 +0100

    Clean code
    Mods: variables, formatting

--- digits.cs
+++ digits.cs
@@ -1,35 +1,51 @@
+
+
 using System;
 
 class digits
 {
- static void Main()
- {
-	 //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-     int a = 0, b = 0, c = 0, d = 0;
-      Console.Write("Insert a number: ");
-      // read the number as string to check the word "end"
-      string e = Console.ReadLine();
-     while(e!="end"){
-int input = Convert.ToInt32(e);
-       if(input != 0)
-{
-       if(input/10 == 0)
-           a ++;  // it only has one digit
-       else 
-       if (input/100 == 0)
-		b ++;  // the number has 2 digits
-        else
-        if (input/1000 == 0)
-        c ++;  // the number has 3 digits
-        else
-           d++;  // the number has more than 3 digits
-        }
-      e = Console.ReadLine();
-}
-
-Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
-
- }
+    static void Main()
+    {
+        int oneDigitCounter = 0, twoDigitsCounter = 0, threeDigitsCounter = 0;
+        int moreDigitsCounter = 0;
+        
+        Console.Write("Insert a number: ");
+        string input = Console.ReadLine();
+        
+        while( input != "end" )
+        {
+            int inputConverted = Convert.ToInt32(input);
+            
+            if(inputConverted != 0)
+            {
+                if( inputConverted / 10 == 0 )
+                {
+                    oneDigitCounter ++;  // it only has one digit
+                }
+                else 
+                {
+                    if ( inputConverted / 100 == 0 )
+                    {
+                        twoDigitsCounter ++;  // the number has 2 digits
+                    }
+                    else
+                    {
+                        if ( inputConverted / 1000 == 0 )
+                        {
+                            threeDigitsCounter ++;  // the number has 3 digits
+                        }
+                        else
+                        {
+                            moreDigitsCounter++;  // the number has more than 3 digits
+                        }
+                    }
+                }
+                input = Console.ReadLine();
+                }
+            }
+            Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", 
+                oneDigitCounter, twoDigitsCounter, threeDigitsCounter, 
+                moreDigitsCounter); 
+    }
 }
 
